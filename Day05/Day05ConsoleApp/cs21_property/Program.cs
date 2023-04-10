f8f3b2b6967c7515c45e9505426251a1d29ee984
llsuzn <dltnwls0987522@gmail.com>
2023-04-10 17:20:25 +0900

    4월10일

---
+++ Day05/Day05ConsoleApp/cs21_property/Program.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Globalization;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cs21_property
+{
+    class Car
+    {
+        int year;
+        string fuelType;
+        string company;
+        int door;
+        int price;
+
+
+        //  필터링이 필요없으면 멤버변수 없이 프로퍼티로 사용
+        public string Name { get; set; }
+        public string Color { get; set; }
+        //  들어오는 데이터를 필터링 할 땐 private 멤버변수와 public 프로퍼트 둘 다 사용
+        public int Year
+        {
+            get { return year; } // ==> get => year; (람다식) 
+            set
+            {
+                if (value <= 1900 || value >= 2023)
+                {
+                    value = 2023;
+                }
+                else
+                {
+                    Year = value;
+                }
+            }
+        }
+        public string FuelType
+        {
+            get => fuelType;
+            set
+            {
+                if (value != "휘발유" || value != "경유")
+                {
+                    value = "휘발유";
+                }
+                fuelType = value;
+            }
+        }
+        public int Door 
+        { 
+            get { return door; } 
+            set
+            {
+                if (value != 2 || value != 4)
+                {
+                    value = 4;
+                }
+                door = value;
+            } 
+        }
+        public string TireType { get; set; }
+        public string Company { get; set; } = "현대자동차";
+        public int Price { get => price; set => price = value; }
+        public string CarIdNumber { get; set; }
+        public string CarPlateNumber { get; set; }
+    }
+
+    interface IProduct
+    {
+        string ProductName { get; set; }
+
+        void Produce();
+    }
+
+    class MyProduct:IProduct
+    {
+        private string productName;
+
+        public string ProductName { 
+            get { return productName; }
+            set { productName = value; }
+        }
+
+        public void Produce()
+        {
+            Console.WriteLine("{0}을(를) 생산합니다.", ProductName);
+        }
+    }
+      
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car genesis = new Car();
+            genesis.Color = "red";
+            Console.WriteLine(genesis.Color);
+            genesis.FuelType = "올리브유";
+            Console.WriteLine(genesis.FuelType);
+
+            // 생성할 때 프로퍼티로 초기화
+            Car K9 = new Car()
+            {
+                Name = "케이나인",
+                FuelType = "식용유",
+                Color = "검정색",
+                Door = 4,
+                TireType = "광폭타이어",
+                Year = 0,
+            };
+
+            Console.WriteLine("자동차 제조회사는 {0}",K9.Company);
+            Console.WriteLine("자동차 제조년도는 {0}년",K9.Year);
+            Console.WriteLine("자동차 연료는 {0}",K9.FuelType);
+            Console.WriteLine("자동차 문 개수 {0}개", K9.Door);
+
+        }
+    }
+}
---
+++ Day05/Day05ConsoleApp/cs22_collection/Program.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cs22_collection
+{
+    class MyList
+    {
+        int[] array;
+        public MyList() 
+        { 
+            array = new int[3];        
+        }
+
+        public int Length
+        {
+            get { return array.Length; }
+        }
+
+        //인덱서
+        public int this[int index]
+        {
+            get
+            {
+                return array[index];
+            }
+            set
+            {
+                if(index >= array.Length)   // 3보다 커지면
+                {
+                    Array.Resize<int>(ref array, index + 1);
+                    Console.WriteLine("MyList Resized : {0} ", array.Length);
+                }
+
+                array[index] = value;       // 값 할당!
+            }
+        }
+    }
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[5];
+            array[0] = 1;
+            array[1] = 2;
+            array[2] = 3;
+            array[3] = 4;
+            array[4] = 5;
+
+            //Console.WriteLine(array[5]);        // IndexOutOfRangeException
+
+            char[] oldString = new char[5];     // 문자열 길이 지정해야하니 C에서만 사용
+            string currString = "";             // 문자열 길이 제한 없음
+
+            ArrayList list = new ArrayList();   // 사이즈 제한 없음
+            list.Add(1);
+            list.Add(2);
+            list.Add(3);
+            list.Add(4);
+            list.Add(5);
+            list.Add(6);
+
+            ArrayList list2 = new ArrayList();
+            list2.Add(1);
+            list2.Add(2);
+            list2.Add(11);
+            list2.Add(33);
+            list2.Add(3);
+            list2.Add(22);
+            list2.Add(50);
+            list2.Add(40);
+
+            foreach (var item in list2)
+            {
+                Console.WriteLine(item);
+            }
+
+            // list에서 데이터 삭제
+            Console.WriteLine("숫자 40 삭제 후");
+            list2.Remove(40);
+            foreach (var item in list2)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("인덱스 3번 데이터 삭제");
+            list2.RemoveAt(3);
+            foreach (var item in list2)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("0번째 위치 7 추가");
+            list2.Insert(0, 7);
+            foreach (var item in list2)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("오름차순 정렬");
+            list2.Sort();
+            foreach (var item in list2)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("내림차순 정렬");
+            list2.Reverse();
+            foreach (var item in list2)
+            {
+                Console.WriteLine(item);
+            }
+
+
+
+            // 새로만든 MyList
+            MyList myList = new MyList();
+            for (int i = 1;i<=5;i++)
+            {
+                myList[i] = i * 5;      // 5, 10, 15, 20 ,,,
+            }
+            for(int i=0;i<myList.Length;i++)
+            {
+                Console.WriteLine(myList[i]);
+            }
+
+            
+        }
+    }
+}
